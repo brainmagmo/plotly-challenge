89a7b52517c5086a59fc06efccad7cf7877f820d
Trevor Kleinstuber <trevor.kleinstuber@gmail.com>
2020-09-14 15:47:53 -0700

    apperently the json can't be accessed outside the .then()?

--- static/js/app.js
+++ static/js/app.js
@@ -1,27 +1,25 @@
 //D3
 //Bootstrap
-var jsonData;
 
 var dropDown = d3.select("#selDataset")
 
-d3.json("samples.json").then(function(data) {
+d3.json("samples.json").then(function(jsonData) {
    
-    data.names.forEach(name => {
+    jsonData.names.forEach(name => {
         console.log(name);
         var option = dropDown.append("option");
         option.text(name).attr("value", name);
     });
 
-    jsonData = data;
+        
+    var names = jsonData.names;
+    var metadata = jsonData.metadata;
+    //var samples = jsonData.samples;
+
+    var idNum = names[0];
+    function getMetaData(idN) {
+        return metadata.find(meta => meta.id === idN);
+    }
+
+    console.log(getMetaData(idNum));
 });
-
-var names = jsonData.names;
-var metadata = jsonData.metadata;
-var samples = jsonData.samples;
-
-var idNum = names[0];
-function getMetaData(idN) {
-    return metadata.find(meta => meta.id === idN);
-}
-
-console.log(getMetaData(idNum));
