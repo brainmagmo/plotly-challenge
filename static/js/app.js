864046ad181d39a80ca362ef33497403091857da
Trevor Kleinstuber <trevor.kleinstuber@gmail.com>
2020-09-14 15:27:44 -0700

    debugging dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,5 @@
 //D3
 //Bootstrap
-//with code from https://stackoverflow.com/questions/43121679/how-to-append-option-into-select-combo-box-in-d3
 
 d3.json("samples.json").then(function(data) {
     var names = data.names;
@@ -8,16 +7,12 @@ d3.json("samples.json").then(function(data) {
     var samples = data.samples;
 
     var dropDown = d3.select("#selDataset")
-    var options = dropDown.selectAll("option")
-                            .data(names)
-                            .enter()
-                            .append("option");
-    options.text(function(d) {
-        return d.NAME;
-         })
-           .attr("value", function(d) {
-        return d.NAME;
-        });
+    
+    name.forEach(name => {
+        console.log(name);
+        var option = dropDown.append("option");
+        option.text(name).attr("value", name);
+    });
 
 
 
