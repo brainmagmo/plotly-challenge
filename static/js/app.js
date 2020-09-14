ceacee6c28912c44e93fd5de87c8f54527c56fbd
Trevor Kleinstuber <trevor.kleinstuber@gmail.com>
2020-09-14 15:06:26 -0700

    populate drop-down menu

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,29 @@
 //D3
 //Bootstrap
+//with code from https://stackoverflow.com/questions/43121679/how-to-append-option-into-select-combo-box-in-d3
 
-var data = d3.json("../samples.json").then(stuff => console.log(stuff));
+d3.json("samples.json").then(function(data) {
+    var names = data.names;
+    var metadata = data.metadata;
+    var samples = data.samples;
+
+    var dropDown = d3.select("#selDataset")
+    var options = dropDown.selectAll("option")
+                            .data(names)
+                            .enter()
+                            .append("option");
+    options.text(function(d) {
+        return d.NAME;
+         })
+           .attr("value", function(d) {
+        return d.NAME;
+        });
+
+
+
+
+
+
+
+});
 
