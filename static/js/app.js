57bfc75f1e2eb1be97397bdc7b028d7b79e10177
Trevor Kleinstuber <trevor.kleinstuber@gmail.com>
2020-09-15 20:44:49 -0700

    style and formatting

--- static/js/app.js
+++ static/js/app.js
@@ -15,20 +15,21 @@ d3.json("samples.json").then(function(jsonData) {
         .enter()
         .append("option")
         .text((d)=>d)
-        .attr("value",(d)=>d);
+        .attr("value",(d)=>d)
+        ;
 
     //get metadata
     var mdDisplay = d3.select("#sample-metadata");
     var idNum = d3.select("#selDataset").property("value");
-    function getMetaData(idN) {
-        return metadata.find(meta => meta.id == idN);//comparing int to string
-    }
-    var md = getMetaData(idNum);
+    function getMetaData(n) {
+        return metadata.find(meta => meta.id == n);//comparing int to string
+    };
+    var metaData = getMetaData(idNum);
 
     //fill default metadata panel
     var subjectMDtext = "";
-    for(var key in md) {
-        subjectMDtext += key + ": " + md[key] + "<br>";
+    for(var key in metaData) {
+        subjectMDtext += key + ": " + metaData[key] + "<br>";
     };
     mdDisplay.html(subjectMDtext);
 
@@ -41,41 +42,47 @@ d3.json("samples.json").then(function(jsonData) {
 
     * Use `otu_labels` as the hovertext for the chart. */
 
-        // Sort the data by Greek search results
-    //var sortedByGreekSearch = data.sort((a, b) => b.greekSearchResults - a.greekSearchResults);
     var sample = jsonData.samples
-                            .find(samp => samp.id == idNum)
-                            ;
-    console.log(sample);
-    // Trace1 for the Greek Data
+        .find(samp => samp.id == idNum)
+        ;
+    
+    // Trace1 for the Sample Data for bar chart
     var trace1 = {
         x: sample.sample_values.slice(0,10).reverse(),
-        y: sample.otu_ids.slice(0,10).reverse().map(num => "OTU" + num.toString(10)),
+        y: sample.otu_ids.slice(0,10).reverse().map(num => "OTU " + num.toString(10)),
         text: sample.otu_labels.slice(0,10).reverse(),
         name: "Samples from " + idNum,
         type: "bar",
         orientation: "h"
     };
-    console.log(trace1.y);
-    // data
-    var data = [trace1];
+    
+    //data
+    var data1 = [trace1];
 
-    // Apply the group bar mode to the layout
     var layout = {
     title: "Belly Button Sample",
-        // margin: {
-        //     l: 100,
-        //     r: 100,
-        //     t: 100,
-        //     b: 100
-        // }
     };
 
     // Render the plot to the div tag with id "plot"
-    Plotly.newPlot("bar", data, layout);
+    Plotly.newPlot("bar", data1, layout);
     //generate bubble chart
 
+    // Trace2 for the Sample Data for bubble chart
+    var trace2 = {
+        x: sample.sample_values.slice(0,10).reverse(),
+        y: sample.otu_ids.slice(0,10).reverse().map(num => "OTU " + num.toString(10)),
+        text: sample.otu_labels.slice(0,10).reverse(),
+        name: "Samples from " + idNum,
+        // type: "bar",
+        // orientation: "h"
+    };
+    
+    //data
+    var data2 = [trace1];
 
+    // Render the plot to the div tag with id "plot"
+    Plotly.newPlot("bubble", data2, layout);
+    //generate bubble chart
     console.log("end of test");
 });
 
